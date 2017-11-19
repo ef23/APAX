f388038d0d3bf42ff19b9927726b2786b53d7e90
Eric Feng <evf23@cornell.edu>
2017-11-19 16:50:58 -0500

    client runs async now

--- client.ml
+++ client.ml
@@ -1,3 +1,5 @@
+(* DO NOT OPEN ASYNC OR CORE BECAUSE THAT WILL OVERWRITE THE STANDARD UNIX LIBRARY *)
+
 let main_client client_fun server_addr server_port_num=
    (* if Array.length Sys.argv < 3
    then Printf.printf "usage :  client server port\n"
@@ -18,29 +20,34 @@ let main_client client_fun server_addr server_port_num=
            with Failure("int_of_string") -> Printf.eprintf "bad port number";
                                             exit 2 ;;
 
-let rec jej x = print_endline "hi"; Async.upon (Async.after (Core.Time.Span.create ~ms:1000 ())) (jej);;
+let rec jej x = 
+  print_endline "hi"; 
+  Async.upon (Async.after (Core.Time.Span.create ~ms:1000 ())) (jej)
+
+let rec query_server ic oc = 
+  try
+    print_string  "Request : ";
+    print_string (string_of_float (Unix.time ()));
+    flush Pervasives.stdout;
+    (* output_string oc ((input_line Pervasives.stdin)^"\n") ; *)
+    output_string oc (("read")^"\n"); (*make client automatically do stuff. can reuse in server code*)
+    output_string oc (("inc")^"\n");
+    flush oc;
+    let r = input_line ic
+    in Printf.printf "Response : %s\n\n" r;
+    flush oc;
+    if r = "END" then (Unix.shutdown_connection ic; raise Exit);
+    Async.upon (Async.after (Core.Time.Span.create ~ms:1000 ())) (fun _ -> (query_server ic oc));
+  with
+    | Exit -> exit 0
+    | exn -> Unix.shutdown_connection ic ; raise exn
 
 let client_fun ic oc =
   (* this would be a heartbeat thing *)
   Async.upon (Async.after (Core.Time.Span.create ~ms:1000 ())) (jej);
-  Async.Scheduler.go ();
-   try
-     while true do
-       (* if (!timer.it_value = 0.)  then begin *)
-       print_string  "Request : " ;
-       print_string (string_of_float (Unix.time ()));
-       flush Pervasives.stdout ;
-       (* output_string oc ((input_line Pervasives.stdin)^"\n") ; *)
-       output_string oc (("read")^"\n") ; (*make client automatically do stuff. can reuse in server code*)
-       output_string oc (("inc")^"\n") ;
-       flush oc ;
-       let r = input_line ic
-       in Printf.printf "Response : %s\n\n" r;
-       flush oc ;
-       if r = "END" then ( Unix.shutdown_connection ic ; raise Exit) ;
-     done
-   with
-       Exit -> exit 0
-     | exn -> Unix.shutdown_connection ic ; raise exn
-
-main_client "10.132.10.130" "9000"
+  
+  (* this would be a main func thing *)
+  Async.upon (Async.after (Core.Time.Span.create ~ms:1000 ())) (fun _ -> (query_server ic oc));
+  Async.Scheduler.go ()
+   
+let _ = main_client client_fun "10.132.10.130" "9000"
