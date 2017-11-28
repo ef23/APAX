7fe210a608da55559e33c7538af218779bbe861a
Eric Feng <evf23@cornell.edu>
2017-11-27 22:04:03 -0500

    rearrange file to account for winning election

--- server.ml
+++ server.ml
@@ -81,6 +81,7 @@ let req_request_vote ballot ip_address_str =
         "last_log_term": ballot.last_log_term
       }
     |} in ()
+
 let res_request_vote msg ip_address_str = failwith "succ my zucc"
 
 (* [handle_vote_req msg] handles receiving a vote request message *)
@@ -107,11 +108,111 @@ let handle_vote_req msg =
         (* send to server *)
     | None -> failwith "kek"
 
+let set_term i =
+    {!serv_state with currentTerm = i}
+
+(* [send_rpcs f ips] recursively sends RPCs to every ip in [ips] using the
+ * partially applied function [f], which is assumed to be one of the following:
+ * [req_append_entries msg]
+ * [req_request_vote msg] *)
+let rec send_rpcs f ips =
+    match ips with
+    | [] -> ()
+    | ip::t ->
+        let _ = f ip in
+        send_rpcs f t
+
+(* [get_entry_term e_opt] takes in the value of a state's last entry option and
+ * returns the last entry's term, or -1 if there was no last entry (aka the log
+ * is empty) *)
+let get_entry_term e_opt =
+    match e_opt with
+    | Some e -> e.entryTerm
+    | None -> -1
+
+(* [start_election ()] starts the election for this server by incrementing its
+ * term and sending RequestVote RPCs to every other server in the clique *)
+let start_election () =
+    (* increment term *)
+    let curr_term = !serv_state.currentTerm in
+    serv_state := set_term (curr_term + 1);
+
+    let neighbors = !serv_state.neighboringIPs in
+    (* ballot is a vote_req *)
+    let ballot = {
+        term = !serv_state.currentTerm;
+        candidate_id = !serv_state.id;
+        last_log_index = !serv_state.commitIndex;
+        last_log_term = get_entry_term (!serv_state.lastEntry)
+    } in
+    send_rpcs (req_request_vote ballot) neighbors
+
+(* let dummy_get_oc ip = failwith "replace with what maria and janice implement"
+let rec send_all_heartbeats ips =
+    match ips with
+    | [] -> ()
+    | h::t ->
+        let oc = dummy_get_oc h in
+        (* TODO defer this? *)
+        send_heartbeat oc ();
+        send_all_heartbeats t *)
+
+(* [init_heartbeats ()] starts a new thread to periodically send heartbeats *)
+let rec init_heartbeats () = ()
+
+(* [act_leader ()] executes all leader responsibilities, namely sending RPCs
+ * and listening for client requests
+ *
+ * if a leader receives a client request, they will process it accordingly *)
+and act_leader () =
+    (* start thread to periodically send heartbeats *)
+    init_heartbeats ()
+    (* TODO listen for client req and send appd_entries *)
+
+(* [act_candidate ()] executes all candidate responsibilities, namely sending
+ * vote requests and ending an election as a winner/loser/stall
+ *
+ * if a candidate receives a client request, they will reply with an error *)
+and act_candidate () =
+    serv_state := {!serv_state with heartbeat = generate_heartbeat};
+    start_election;
+    (* call act_candidate again if timer runs out *)
+    (* now listen for responses to the req_votes *)
+    let rec listen () =
+        if !vote_counter > ((List.length !serv_state.neighboringIPs) / 2) then
+            win_election ()
+        (* TODO this is probably not correct *)
+        else listen ()
+    in listen ()
+
+(* [act_follower ()] executes all follower responsibilities, namely starting
+ * elections, responding to RPCs, and redirecting client calls to the leader
+ *
+ * if a follower receives a client request, they will send it as a special RPC
+ * to the leader, and then receive the special RPC and reply back to the client
+ *)
+and act_follower () = failwith "TODO"
+
+(* [win_election ()] transitions the server from a candidate to a leader and
+ * executes the appropriate actions *)
+and win_election () =
+    (* transition to Leader role *)
+    serv_state := {!serv_state with role = Leader};
+    (* send heartbeats *)
+    act_leader ()
+
+and lose_election () =
+    (* transition to Follower role *)
+    serv_state := {!serv_state with role = Follower};
+    act_follower ()
+
 (* [handle_vote_res msg] handles receiving a vote response message *)
 let handle_vote_res msg =
     let currTerm = msg |> member "current_term" |> to_int in
     let voted = msg |> member "vote_granted" |> to_bool in
-    if voted then vote_counter := !vote_counter + 1
+    if voted then vote_counter := !vote_counter + 1;
+    let majority = ((List.length !serv_state.neighboringIPs) / 2) + 1 in
+    if !vote_counter >= majority then win_election ()
 
 let handle_message msg =
 
@@ -126,10 +227,10 @@ let handle_message msg =
     | "appd_res" -> "gug"
     | _ -> "gug"
 
+
 let rec send_heartbeat oc () =
     Lwt_io.write_line oc "test"; Lwt_io.flush oc;
     Async.upon (Async.after (Core.Time.Span.create ~ms:1000 ())) (send_heartbeat oc) (*TODO test with not hardcoded values for heartbeat*)
-
 
 let rec handle_connection ic oc () =
     Lwt_io.read_line_opt ic >>=
@@ -181,100 +282,7 @@ let create_server sock =
  *)
         establish_conn "";
         Lwt_unix.accept sock >>= accept_connection >>= serve
-
     in serve
-
-let set_term i =
-    {!serv_state with currentTerm = i}
-
-(* [send_rpcs f ips] recursively sends RPCs to every ip in [ips] using the
- * partially applied function [f], which is assumed to be one of the following:
- * [req_append_entries msg]
- * [req_request_vote msg] *)
-let rec send_rpcs f ips =
-    match ips with
-    | [] -> ()
-    | ip::t ->
-        let _ = f ip in
-        send_rpcs f t
-
-(* [get_entry_term e_opt] takes in the value of a state's last entry option and
- * returns the last entry's term, or -1 if there was no last entry (aka the log
- * is empty) *)
-let get_entry_term e_opt =
-    match e_opt with
-    | Some e -> e.entryTerm
-    | None -> -1
-
-(* [start_election ()] starts the election for this server by incrementing its
- * term and sending RequestVote RPCs to every other server in the clique *)
-let start_election () =
-    (* increment term *)
-    let curr_term = !serv_state.currentTerm in
-    serv_state := set_term (curr_term + 1);
-
-    let neighbors = !serv_state.neighboringIPs in
-    let req_vote_json = build_req_vote () in
-    send_rpcs (req_request_vote req_vote_json) neighbors
-
-(* let dummy_get_oc ip = failwith "replace with what maria and janice implement"
-let rec send_all_heartbeats ips =
-    match ips with
-    | [] -> ()
-    | h::t ->
-        let oc = dummy_get_oc h in
-        (* TODO defer this? *)
-        send_heartbeat oc ();
-        send_all_heartbeats t *)
-
-(* [init_heartbeats ()] starts a new thread to periodically send heartbeats *)
-let rec init_heartbeats () = ()
-
-(* [act_leader ()] executes all leader responsibilities, namely sending RPCs
- * and listening for client requests
- * 
- * if a leader receives a client request, they will process it accordingly *)
-and act_leader () =
-    (* start thread to periodically send heartbeats *)
-    init_heartbeats ()
-    (* TODO listen for client req and send appd_entries *)
-
-(* [act_candidate ()] executes all candidate responsibilities, namely sending
- * vote requests and ending an election as a winner/loser/stall
- *
- * if a candidate receives a client request, they will reply with an error *)
-and act_candidate () =
-    serv_state := {!serv_state with heartbeat = generate_heartbeat};
-    start_election;
-    (* call act_candidate again if timer runs out *)
-    (* now listen for responses to the req_votes *)
-    let rec listen () =
-        if !vote_counter > ((List.length !serv_state.neighboringIPs) / 2) then
-            win_election ()
-        (* TODO this is probably not correct *)
-        else listen ()
-    in listen ()
-
-(* [act_follower ()] executes all follower responsibilities, namely starting
- * elections, responding to RPCs, and redirecting client calls to the leader
- * 
- * if a follower receives a client request, they will send it as a special RPC
- * to the leader, and then receive the special RPC and reply back to the client
- *)
-and act_follower () = failwith "TODO"
-
-(* [win_election ()] transitions the server from a candidate to a leader and 
- * executes the appropriate actions *)
-and win_election () =
-    (* transition to Leader role *)
-    serv_state := {!serv_state with role = Leader};
-    (* send heartbeats *)
-    act_leader ()
-
-and lose_election () =
-    (* transition to Follower role *)
-    serv_state := {!serv_state with role = Follower};
-    act_follower ()
 
 let _ =
     let sock = create_socket () in
