f18e81b24a4b43fef619b472c1779abfaff6d8da
Andy Zhang <jz359@cornell.edu>
2017-12-01 15:12:47 -0500

    fixed bug w lastentryidx

--- server.ml
+++ server.ml
@@ -227,7 +227,7 @@ let process_conflicts entries =
 
 let append_new_entries entries = failwith "Unasdlkjfadsf"
 
-let last_entry_idx = match !serv_state.log with | [] -> 0 | (i,_)::t -> i
+let last_entry_idx () = match !serv_state.log with | (i,_)::t -> i | [] -> 0 
 
 let handle_ae_req msg oc =
     let ap_term = msg |> member "ap_term" |> to_int in
@@ -248,7 +248,7 @@ let handle_ae_req msg oc =
     process_conflicts entries; (* 3 *)
     append_new_entries entries; (* 4 *)
     if leader_commit > !serv_state.commitIndex
-    then serv_state := {!serv_state with commitIndex = min leader_commit last_entry_idx}; (* 5 *)
+    then serv_state := {!serv_state with commitIndex = min leader_commit (last_entry_idx ())}; (* 5 *)
     res_append_entries ae_res oc
 
     (* failwith "parse every json field in AE RPC. follow the receiver implementation in the pdf" *)
